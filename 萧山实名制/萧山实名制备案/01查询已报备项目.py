a878620f1be862dfec57262ac877e0f11b896ccc
zml <zml1120110053@icloud.com>
2021-04-08 01:20:47 +0800

    Default Changelist

--- 萧山实名制/萧山实名制备案/01查询已报备项目.py
+++ 萧山实名制/萧山实名制备案/01查询已报备项目.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import requests
 from requests import HTTPError
 import json
@@ -10,50 +11,27 @@ port = 3306
 user = 'zml'
 pw = 'Aa123456'
 db_name = 'xssmz'
-TableName = 'hz-xs-ybb-project'
+TableName = '`hz-xs-ybb-project`'
 
 # 请求相关信息
-token = '3ac5d38937234362ad09cb650beddb4e'
+token = '8643d34830384e10a7a6d633c41eaf36'
 headers = {
         'Content-Type': 'application/json; charset=utf-8',
         'x-access-token': token}
 pageNo =1
 pageSize = 100
 
-dict = {
-        "address": "南阳街道左十四线与南庄路交叉口向西50米",
-        "appId": "330109210329010002",
-        "buildCorpCode": "11330109002514224B",
-        "buildCorpNames": "杭州市公安局萧山区分局",
-        "builderLicenseNumber": "330109202101080201",
-        "buildingArea": 9460.00,
-        "category": "01",
-        "categoryName": "房屋建筑工程",
-        "city": "杭州市",
-        "contractorCorpCode": "913301096917328679",
-        "contractorCorpNames": "港立建设（浙江）有限公司",
-        "county": "萧山区",
-        "dockCountryPlatform": False,
-        "govOrgNames": "杭州钦汇建设工程管理有限公司淳安分公司",
-        "invest": 2972.6398,
-        "orgName": "萧山区",
-        "projectArea": "浙江省杭州市萧山区南阳街道",
-        "projectCode": "330109210329010002",
-        "projectId": 8481,
-        "projectName": "南阳派出所新建工程项目",
-        "projectStatus": "003",
-        "projectStatusName": "在建",
-        "province": "浙江省",
-        "registerId": "5434",
-        "secretKey": "?igIAb@$R7+h@48Ymyn29r3r8/#DV-lX",
-        "street": "南阳街道",
-        "workerCount": 0
-      }
 
 def main():
-    # xunhuan()
+    # 请求所有数据
+    list_all = xunhuan()
+    # 对所有数据，进行循环写入
+    for i in range(len(list_all)):
+        # print(list_all[i])
+        print(f'正在写入第{i+1}条数据,总共{len(list_all)}条 ', end='')
 
-    InsertData(TableName,dict)
+        # 写入数据
+        InsertData(TableName, list_all[i])
 
 
 # 请求 项目库，返回数据（列表）
@@ -74,52 +52,106 @@ def askUrl(pageNo, pageSize):
         print(f'    请求出错,原因是{e.reason}')
     r.encoding = 'utf-8'
     # print(r.text)  # 打印原始请求数据
-
-    # 输出结果
-    list1 = r.json()['data']['list']
-    total = r.json()['data']['total']
-    totalPage = r.json()['data']['totalPage']
-    print(f'请求结果如下，现请求{pageNo}页，每页{pageSize}条，共{total}条数据，共{totalPage}页')
-    return list1, totalPage
+    if r.json()['code'] == 401:
+        print('token失效，请更新token！！')
+        return r.json()['code'], 0
+    else:
+        list1 = r.json()['data']['list']
+        total = r.json()['data']['total']
+        totalPage = r.json()['data']['totalPage']
+        print(f'请求结果如下，现请求{pageNo}页，每页{pageSize}条，共{total}条数据，共{totalPage}页')
+        return list1, totalPage
 
 def xunhuan():
     list_all = []
     totalPage = 100000
     pageNo = 1
+    # 根据第一次请求，更新totalpage。
     while pageNo <= totalPage:
         list1, totalPage = askUrl(pageNo, pageSize)
-        list_all += list1
-        pageNo += 1
-        time.sleep(random.randint(0, 2))
+        if list1 == 401:
+            break
+        else:
+            list_all += list1
+            pageNo += 1
+            # totalPage = 1
+            time.sleep(random.randint(0, 2))
     print(f'请求完毕,总共有{len(list_all)}条数据')
     return list_all
 
-
-    # # 打印请求结果，并省略原数据
-    # data = r.json()
-    # data['rows'] = '省略原数据'
-    # print("3、请求结果如下：\n%s" % data)
-    # return r.json()
-
-
+# 自定义列表，写入mysql
 def InsertData(TableName, dic):
+    dic['addtime'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    try:
+        db = pymysql.connect(host=host, port=port, user=user, passwd=pw, db=db_name)
+        cur = db.cursor()
+        # print(f'select projectId from {TableName} where projectId = {dic["projectId"]}')
+        n = cur.execute(f'select projectId from {TableName} where projectId = {dic["projectId"]}')
+
+        if n == 0:
+            COLstr = '' # 列的字段
+            ROWstr = '' # 行字段
+            list = [
+                    'appId',
+                    'projectId',
+                    'buildCorpCode',
+                    'buildCorpNames',
+                    'contractorCorpCode',
+                    'contractorCorpNames',
+                    'govOrgNames',
+                    'builderLicenseNumber',
+                    'orgName',
+                    'projectName',
+                    'projectStatus',
+                    'projectStatusName',
+                    'secretKey',
+                    'workerCount',
+                    'addtime'
+                    ]
+            # print(dic)
+            ColumnStyle = ' CHAR(40)'
+            for key in list:
+                COLstr = COLstr + key +','
+                if key in dic:
+                    val = f'"{dic[key]}"'
+                else:
+                    val = '"无"'
+                ROWstr = ROWstr + val + ','
+            # COLstr = COLstr + 'addtime' + ','
+            # ROWstr = ROWstr + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ','
+            cur.execute("SELECT * FROM %s" % TableName)
+            cur.execute(f'INSERT INTO {TableName}({COLstr[:-1]}) VALUES ({ROWstr[:-1]})')
+            print('写入成功！')
+        else:
+            print('已存在！')
+
+        db.commit()
+        cur.close()
+        db.close()
+    except pymysql.Error as e:
+        print(f'  INSERT INTO {TableName}({COLstr[:-1]}) VALUES ({ROWstr[:-1]})')
+        print("Mysql Error %d: %s" % (e.args[0], e.args[1]))
+
+
+
+
+# 无效  字典写入到mysql
+def InsertData1(TableName, dic):
     try:
         db = pymysql.connect(host=host, port=port, user=user, passwd=pw, db=db_name)
         cur = db.cursor()
         COLstr = ''  # 列的字段
         ROWstr = ''  # 行字段
 
-        ColumnStyle = 'CHAR(40)'
+        ColumnStyle = ' CHAR(40)'
         for key in dic.keys():
             COLstr = COLstr + ' ' + key + ColumnStyle + ','
-            print(COLstr)
-            ROWstr = (ROWstr + '"%s"' + ',') % (dic[key])
-            print(ROWstr)
+            ROWstr = (ROWstr + f'"{dic[key]}"' + ',')
 
         # 推断表是否存在，存在运行try。不存在运行except新建表，再insert
         try:
             cur.execute("SELECT * FROM %s" % TableName)
-            cur.execute("INSERT INTO %s VALUES (%s)" % (TableName, ROWstr[:-1]))
+            cur.execute(f'INSERT INTO {TableName}({COLstr[:-1]}) VALUES ({ROWstr[:-1]})')
 
         except pymysql.Error as e:
             cur.execute("CREATE TABLE %s (%s)" % (TableName, COLstr[:-1]))
@@ -127,18 +159,8 @@ def InsertData(TableName, dic):
         db.commit()
         cur.close()
         db.close()
-
     except pymysql.Error as e:
         print("Mysql Error %d: %s" % (e.args[0], e.args[1]))
-
-
-    # db = pymysql.connect(host=host, port=port, user=user, passwd=pw, db=db_name)
-# cursor = db.cursor()
-# cursor.execute("SELECT VERSION()")
-# data = cursor.fetchone()
-# print('Database version : %s' % data)
-# db.close()
-
 
 
 if __name__ == "__main__":  # 当程序执行时
