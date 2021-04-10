cce42fc528163e18df10da57d427c0c6ba28df9c
zml1994 <zml1120110053@icloud.com>
2021-04-10 17:19:44 +0800

    提交

--- 杭州实名制/test2.py
+++ 杭州实名制/test2.py
@@ -1,25 +1,8 @@
-import requests
+from datetime import datetime, timedelta
+# import pandas as pd
 
 
-def askUrl(url,data):
-    headers = {
-        'Content-Type': 'application/json; charset=utf-8',
-        'x-access-token': 'e21e2a2ed5864572ba7eca602554e8d2'}
-    # data = json.dumps(data)   #  序列化成jsong
-    r = requests.post(url=url, json=data, headers=headers)
-    r.encoding = 'utf-8'
-    print(r.json())
-    print(r.text)
-    # print("post执行状态码：%s" % r.json()['code'])
-    # print("post执行结果：%s" % r.json()['msg'])
-    # print("post本页数据总数：%s" % r.json()['data']['currentPageSize'])
-    # print("页码：%s" % r.json()['data']['pageNum'])
-    # print("每页大小：%s" % r.json()['data']['pageSize'])
-    # print("总共%s页" % r.json()['data']['totalPage'])
-    # print("项目总数总数：%s" % r.json()['data']['total'])
-    # data = r.json()['data']
-    # list1 = data['list']
-    # return list1
+data = [(datetime.strptime('2020-12-01', "%Y-%m-%d") + timedelta(days=i)).strftime("%Y-%m-%d") for i in range(62)]
+print(data)
 
-
-askUrl(url1, data4)
+# date = pd.date_range(start,end)
--- 萧山实名制/萧山实名制备案/01查询已报备项目.py
+++ 萧山实名制/萧山实名制备案/01查询已报备项目.py
@@ -5,46 +5,81 @@ import json
 import time
 import pymysql
 import random
+
 # 数据库配置信息
 host = 'rm-bp1ya4u34kwfscg7ido.mysql.rds.aliyuncs.com'
 port = 3306
 user = 'zml'
 pw = 'Aa123456'
 db_name = 'xssmz'
-TableName = '`hz-xs-ybb-project`'
+TableName_hzbb = '`hz-xs-ybb-project`'
+TableName_xsconfigList = '`hz-xs-configList`'
 
-# 请求相关信息
-token = '8643d34830384e10a7a6d633c41eaf36'
+# 请求基础配置
+token = 'b9deb2a9df4147db8f0f6b50ef037c1b'
 headers = {
-        'Content-Type': 'application/json; charset=utf-8',
-        'x-access-token': token}
-pageNo =1
+    'Content-Type': 'application/json; charset=utf-8',
+    'x-access-token': token}
+pageNo_s = 1  # 起始页
 pageSize = 100
+
+# 请求url参数配置
+projectStatus = '003'  # 项目状态
+isRegister = '0'  # 是否报备
+govOrgIds = [14]  # 监管机构，14：萧山区
 
 
 def main():
+    xsbb()
+    xspz()
+
+
+def xspz():
+    print('1、正在发起循环请求数据')
+    list_all = askUrl_configList()
+    print(f'2、请求完毕！总共{len(list_all)}条数据，正在写入数据到数据库')
+
+    # 写入数据
+    r = InsertData(TableName_xsconfigList, list_all)
+
+    print(f'3、数据写入完毕！')
+
+def xsbb():
     # 请求所有数据
-    list_all = xunhuan()
+    print('1、正在发起循环请求数据')
+    list_all = xunhuan(pageNo_s)
     # 对所有数据，进行循环写入
-    for i in range(len(list_all)):
-        # print(list_all[i])
-        print(f'正在写入第{i+1}条数据,总共{len(list_all)}条 ', end='')
-
-        # 写入数据
-        InsertData(TableName, list_all[i])
+    print(f'2、请求完毕！总共{len(list_all)}条数据，正在写入数据到数据库')
+    r = InsertData(TableName_hzbb, list_all)
+    print(f'3、数据写入完毕！\n')
 
 
-# 请求 项目库，返回数据（列表）
-def askUrl(pageNo, pageSize):
-    print(f'正在请求杭州市项目库，第{pageNo}页，{pageSize}条/页')
+
+def askUrl_configList():
+    url = 'http://47.110.228.86:8081/api/push/config/1/10000/configList?platformName=%E6%9D%AD%E5%B7%9E%E5%B9%B3%E5%8F%B0'
+    try:
+        r = requests.get(url, headers=headers)
+    except HTTPError as e:
+        print(e.reason)
+    r.encoding = 'utf-8'
+    # print("3、请求结果原文如下\n%s" % r.text)
+
+    # 打印请求结果，并省略原数据
+    data = r.json()
+    data['data']['list'] = '省略原数据'
+    print(f'3、请求结果如下：\n{data}')
+    return r.json()['data']['list']
+
+# 请求url 杭州市项目库，返回数据（列表）
+def askUrl(pageNo, pageSize, projectStatus, isRegister, govOrgIds):
+    global r
     url = f'http://115.233.209.232:9000/api/realname/project/register/{pageNo}/{pageSize}/findProjectRegister'
     data = {
-        'projectStatus': '003',  # 项目状态
-        'isRegister': '0',  # 是否报备
-        'projectName': '',  # 项目名称
-        'govOrgIds': [14]  # 监管机构，14：萧山区
+        'projectStatus': projectStatus,
+        'isRegister': isRegister,
+        'projectName': '',
+        'govOrgIds': govOrgIds
     }
-
     # 请求异常判断
     try:
         r = requests.post(url, json=data, headers=headers)
@@ -52,87 +87,110 @@ def askUrl(pageNo, pageSize):
         print(f'    请求出错,原因是{e.reason}')
     r.encoding = 'utf-8'
     # print(r.text)  # 打印原始请求数据
+    # 如果请求结果为401，返回401
     if r.json()['code'] == 401:
-        print('token失效，请更新token！！')
         return r.json()['code'], 0
+    # 如果请求结果正常，返回list
     else:
         list1 = r.json()['data']['list']
         total = r.json()['data']['total']
         totalPage = r.json()['data']['totalPage']
-        print(f'请求结果如下，现请求{pageNo}页，每页{pageSize}条，共{total}条数据，共{totalPage}页')
+        print(f'    分页请求: 每页{pageSize}条，请求{pageNo}页')
+        print(f'    返回结果:共{total}条数据，共{totalPage}页\n')
         return list1, totalPage
 
-def xunhuan():
-    list_all = []
-    totalPage = 100000
-    pageNo = 1
-    # 根据第一次请求，更新totalpage。
-    while pageNo <= totalPage:
-        list1, totalPage = askUrl(pageNo, pageSize)
+# 循环请求，返回list_all
+def xunhuan(pageNo_s, totalPage=100000):  # 定义totalPage初始变量，足够大就行
+    list_all = []  # 定义空列表，用于接受查询数据并返回
+
+    # 根据起始页，进行循环请求
+    while pageNo_s <= totalPage:
+        # 发起请求，返回list数据及总共多少页
+        list1, totalPage = askUrl(pageNo_s, pageSize, projectStatus, isRegister, govOrgIds)
+        # 如果返回结果是401，则停止
         if list1 == 401:
-            break
+            print('token失效，请更新token！！')
+        # 返回结果正常，对list进行叠加
         else:
             list_all += list1
-            pageNo += 1
-            # totalPage = 1
-            time.sleep(random.randint(0, 2))
-    print(f'请求完毕,总共有{len(list_all)}条数据')
+            pageNo_s += 1
+            time.sleep(random.randint(0, 10)*0.1)
     return list_all
 
+
 # 自定义列表，写入mysql
-def InsertData(TableName, dic):
-    dic['addtime'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+def InsertData(TableName, list_all):
+    # 将当前时间添加到需要写入的字典中
+    if TableName == '`hz-xs-ybb-project`':
+        list = [
+            'appId',
+            'projectId',
+            'buildCorpCode',
+            'buildCorpNames',
+            'contractorCorpCode',
+            'contractorCorpNames',
+            'govOrgNames',
+            'builderLicenseNumber',
+            'orgName',
+            'projectName',
+            'projectStatus',
+            'projectStatusName',
+            'secretKey',
+            'workerCount',
+            'addtime'
+        ]
+        kk = 'projectId'
+    elif TableName == '`hz-xs-configList`' or TableName == '`test2`' :
+        list = [
+            'myProjectCode',
+            'projectName',
+            'pushAppId',
+            'pushProjectCode',
+            'pushSecurityKey',
+            'pushUrl',
+            'createTime',
+            'addtime'
+        ]
+        kk = 'pushProjectCode'
+    # 对pymysql进行异常判断
     try:
         db = pymysql.connect(host=host, port=port, user=user, passwd=pw, db=db_name)
         cur = db.cursor()
-        # print(f'select projectId from {TableName} where projectId = {dic["projectId"]}')
-        n = cur.execute(f'select projectId from {TableName} where projectId = {dic["projectId"]}')
-
-        if n == 0:
-            COLstr = '' # 列的字段
-            ROWstr = '' # 行字段
-            list = [
-                    'appId',
-                    'projectId',
-                    'buildCorpCode',
-                    'buildCorpNames',
-                    'contractorCorpCode',
-                    'contractorCorpNames',
-                    'govOrgNames',
-                    'builderLicenseNumber',
-                    'orgName',
-                    'projectName',
-                    'projectStatus',
-                    'projectStatusName',
-                    'secretKey',
-                    'workerCount',
-                    'addtime'
-                    ]
-            # print(dic)
-            ColumnStyle = ' CHAR(40)'
-            for key in list:
-                COLstr = COLstr + key +','
-                if key in dic:
-                    val = f'"{dic[key]}"'
-                else:
-                    val = '"无"'
-                ROWstr = ROWstr + val + ','
-            # COLstr = COLstr + 'addtime' + ','
-            # ROWstr = ROWstr + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ','
-            cur.execute("SELECT * FROM %s" % TableName)
-            cur.execute(f'INSERT INTO {TableName}({COLstr[:-1]}) VALUES ({ROWstr[:-1]})')
-            print('写入成功！')
-        else:
-            print('已存在！')
-
+        t = 0
+        f = 0
+        for dic in list_all:
+            dic['addtime'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        # 根据projectId查找数据库是否存在！
+            keyword = dic[kk]
+        # print(f'select {kk} from {TableName} where {kk} = {keyword}')
+            n = cur.execute(f'select {kk} from {TableName} where {kk} = \'{keyword}\'')
+        # 数据库不存在，构建写入数据库的列名称、行内容字段
+            if n == 0:
+                COLstr = ''  # 列的字段
+                ROWstr = ''  # 行字段
+                # 根据自定义字段，循环构建列名称、行内容字段
+                for key in list:
+                    COLstr = COLstr + key + ','
+                    # 如果key在字典中存在，写入value；不存在，写入”无“
+                    if key in dic:
+                        val = f'"{dic[key]}"'
+                    else:
+                        val = '"无"'
+                    ROWstr = ROWstr + val + ','
+                # 执行sql，写入数据
+                cur.execute(f'INSERT INTO {TableName}({COLstr[:-1]}) VALUES ({ROWstr[:-1]})')
+                t += 1
+                # print(f't={t}')
+            else:
+                f += 1
+                # print(f'f={f}')
+        print(f'新增{t}条数据，{f}条数据已存在！')
         db.commit()
         cur.close()
         db.close()
     except pymysql.Error as e:
-        print(f'  INSERT INTO {TableName}({COLstr[:-1]}) VALUES ({ROWstr[:-1]})')
         print("Mysql Error %d: %s" % (e.args[0], e.args[1]))
-
-
+        print(f'执行的sql语句： INSERT INTO {TableName}({COLstr[:-1]}) VALUES ({ROWstr[:-1]})')
 
 
 # 无效  字典写入到mysql
@@ -168,4 +226,4 @@ if __name__ == "__main__":  # 当程序执行时
     time_start = time.time()
     main()
     t = time.time() - time_start
-    print(f'End：程序执行完毕，总共执行时间：{int(t / 60)}分 {int(t % 60)}秒!')
+    print(f'\nEnd：程序执行完毕！总共执行时间：{int(t / 60)}分 {int(t % 60)}秒!')
